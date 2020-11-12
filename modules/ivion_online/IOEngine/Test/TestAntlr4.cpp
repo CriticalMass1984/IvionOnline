3af7f4420bb934ec601414d8301509d3e673f9bc
Zack Dunnewind <zack.dunnewind@gmail.com>
2020-11-12 10:50:43 -0500

    .

--- modules/ivion_online/IOEngine/Test/TestAntlr4.cpp
+++ modules/ivion_online/IOEngine/Test/TestAntlr4.cpp
@@ -3,12 +3,9 @@
 #include <IOEngine/Antlr4/IvionParser.h>
 #include <antlr4-runtime.h>
 
-#include <IOEngine/AST/AST.hpp>
-#include <IOEngine/Card.hpp>
-#include <IOEngine/GameInstance.hpp>
-#include <IOEngine/Player.hpp>
-#include <IOEngine/Program.hpp>
 
 int main(int argc, char **argv) {
-	
+	IvionParser parser;
+	Ivion
+	IvionBaseVisitor visitor;
 }
